8323979993b03ad3444ecb3f6d1d814ccd39cd23
siriveeramalla <siriveeramalla4@gmail.com>
2025-10-18 18:15:11 +0530

    changed completely

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static(__dirname));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, 'library.html'));
 });
 
 app.listen(PORT, () => {
